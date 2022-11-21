0cf256531ba911899b48033b07d9c0a8d5fe7491
WilliamPB07 <101308021+WilliamPB07@users.noreply.github.com>
2022-11-21 09:23:16 -0600

    common things

---
+++ Parqueo/src/main/java/com/mycompany/parqueo/modelos/Camion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.parqueo.modelos;
+
+/**
+ *
+ * @author wpena
+ */
+public class Camion extends Vehiculo
+{
+    public Camion(String placa, String marca, String modelo, int annoFabricacion)
+    {
+        //Indica a la clase padre que debe construirse como el tipo Sedan 
+        super(TiposVehiculo.CAMION);
+        
+        //Asigna valores de las Propiedades
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.annoFabricacion = annoFabricacion;
+    }
+    
+}
